9886bcb3aeca49b0be98c4ed7b07113703f0497f
adityakumartraveloka <aditya.kumar@traveloka.com>
2020-02-04 00:24:52 +0530

    Create and Updates are done

--- project/register_request/serializers.py
+++ project/register_request/serializers.py
@@ -25,12 +25,27 @@ class RegisterAccessRequestSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         print('[serializers.py]',validated_data)
-        product_domains = validated_data.pop('product_domains')
+        product_domains_data = validated_data.pop('product_domains')
         request_access = RegisterAccessRequest.objects.create(**validated_data)
 
-        for name in product_domains:
-            print(name)
+        for product_domain in product_domains_data:
+            product_domain, created = ProductDomain.objects.get_or_create(name=product_domain['name'])
+            request_access.product_domains.add(product_domain)
         return request_access
+
+    def update(self, instance, validated_data):
+        product_domains_data = validated_data.pop('product_domains')
+        instance.email =  validated_data.get('email', instance.email)
+        instance.designation = validated_data.get('designation', instance.designation)
+
+        product_domain_list = []
+
+        for product_domain in product_domains_data:
+            product_domain, created = ProductDomain.objects.get_or_create(name=product_domain['name'])
+            product_domain_list.append(product_domain)
+        instance.product_domains.add(*product_domain_list)
+        instance.save()
+        return instance
 
     def __str__(self):
         return self.email
